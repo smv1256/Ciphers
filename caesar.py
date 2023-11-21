abce78e5ad7391098cf37d3f859626dbe5cb1dc5
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2023-11-21 09:31:53 -0500

    Create caesar.py

---
+++ caesar.py
@@ -0,0 +1,25 @@
+# yo I did it
+# see I was lazy and then I needed a Ceasar decrypter for CyberStart
+# ngl I would feel mildly fancy if this weren't Python
+# but there's a built-in Python IDE you need to use rip lol
+
+def caesar(key, message, mode):
+  m = message.upper()
+  a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  output = ""
+
+  for l in m: # letter in message
+    if l in a: # if letter is in the alphabet
+      match mode:
+        case "encrypt": 
+          ind = (a.find(l) + key) % len(a) # shift + to find ciphertext letter
+        case "decrypt":
+          ind = (a.find(l) - key) % len(a) # shift - to find original letter
+        case _:
+          print("invalid mode")
+      output = output + a[ind]
+    else: # if it's a symbol, keep it
+      output = output + l
+
+  return output
+  
